2ff3bef84fdd58fffd977a5e96c818c3b7e6dc12
DaMalle <marcfriis@protonmail.com>
2021-07-05 15:58:38 -0700

    testing canvas in layout

--- sudoku/data/sudoku_solution_data.py
+++ sudoku/data/sudoku_solution_data.py
@@ -3,9 +3,9 @@
 
 class SudokuSolution:
     def __init__(self, random_lib):
-        self.baseline = 3
-        self.grid_len = self.baseline**2
-        self.grid = self.grid_len**2
+        self._BASE = 3
+        self._SIDE = 9
+        self._GRID = 81
         
         # Libaries
         self.random_lib = random_lib
@@ -13,21 +13,19 @@ class SudokuSolution:
     def create(self):
         """creates valid sudoku solution"""
 
-        self.shuffled_baseline = self.shuffle(list(range(self.baseline)))
-
         # generates and shuffles row- & column-numbers in groups, based on the length of the tile. example: [|3, 1, 2,| 6, 5, 4,|7, 9, 8|]
-        self.rows = [ group * self.baseline + row for group in self.shuffled_baseline for row in self.shuffled_baseline ]
-        self.columns = [ group * self.baseline + column for group in self.shuffled_baseline for column in self.shuffled_baseline ]
+        self._row_order = [ g * self._BASE + r for g in self.shuffle(range(self._BASE)) for r in self.shuffle(range(self._BASE)) ]
+        self._col_order = [ g * self._BASE + c for g in self.shuffle(range(self._BASE)) for c in self.shuffle(range(self._BASE)) ]
 
-        self.shuffled_numbers = self.shuffle(list(range(1, self.grid_len + 1)))
+        self._shuffled_nums = self.shuffle(range(1, 10))
 
         # returns a valid grid, based on the pattern-algorithm
-        return [ [ self.shuffled_numbers[self.apply_pattern(row, column)] for column in self.columns ] for row in self.rows ]
+        return [ [ self._shuffled_nums[self.apply_pattern(row, col)] for col in self._col_order ] for row in self._row_order ]
 
     # support/internal functions
     def shuffle(self, data):
         return self.random_lib.sample(data, len(data))
     
-    def apply_pattern(self, row, column):
+    def apply_pattern(self, row, col):
         """creates a default board by rotating cells in the tile by 3 and then by 1 more for each new tile"""
-        return (self.baseline * (row % self.baseline) + row // self.baseline + column) % self.grid_len
+        return (self._BASE * (row % self._BASE) + row // self._BASE + col) % self._SIDE
